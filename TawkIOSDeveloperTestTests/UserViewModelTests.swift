97ba33693064294f99c286b0c099adffb1223825
anthonyfullwood <105413187+anthonyfullwood@users.noreply.github.com>
2022-11-18 14:41:45 -0500

    Update UserViewModelTests.swift

--- TawkIOSDeveloperTestTests/UserViewModelTests.swift
+++ TawkIOSDeveloperTestTests/UserViewModelTests.swift
@@ -12,28 +12,6 @@ final class UserViewModelTests: XCTestCase {
 
 
     
-    /* Note that this unit test is not working becuase I just could not get it to work
-       with Core Data and unit testing is new to me but I wanted to show that
-       I am interested in learning how to do it properly and that I have basic understanding of
-       what it is and its importance
-     */
-    
-    private var userVM: UserViewModel!
-    
-    override func setUp() {
-    
-     
-        self.userVM = UserViewModel()
-        self.userVM.users = []
-        
-    }
-    
-    func test_should_return_a_value(){
-        
-        let result = userVM.cellForRowAt(5)
-        
-        XCTAssertNotNil(result)
-        
-    }
+   
     
 }
